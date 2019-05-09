8aa3702bfbf2449cf30ea690ddd27e6913b597a5
Stephen Barry <sbarry@trustev.com>
2019-05-09 14:30:27 +0100

    Add additional PaymentType enum for Other

--- src/main/java/com/trustev/domain/entities/PaymentType.java
+++ src/main/java/com/trustev/domain/entities/PaymentType.java
@@ -1,5 +1,5 @@
 package com.trustev.domain.entities;
 
 public enum PaymentType {
-	None, CreditCard, DebitCard, DirectDebit, PayPal, Bitcoin, Due, Stripe, Dwolla, ApplePay, Payoneer, TwoCheckout, AmazonPay, Square, AndroidPay, LeoPay, GooglePay, SamsungPay, MasterPass, MicrosoftPay
+	None, CreditCard, DebitCard, DirectDebit, PayPal, Bitcoin, Due, Stripe, Dwolla, ApplePay, Payoneer, TwoCheckout, AmazonPay, Square, AndroidPay, LeoPay, GooglePay, SamsungPay, MasterPass, MicrosoftPay, Other
 }
